f4b6ee5464feffee91949e5d100361c0982991d5
JonahEgashira <jonahega@gmail.com>
2021-10-14 12:03:15 +0900

    Add ABS parser

--- 9cc.c
+++ 9cc.c
@@ -47,6 +47,44 @@ Node *new_node_num(int val) {
   return node;
 }
 
+Node *expr() {
+  Node *node = mul();
+
+  for (;;) {
+    if (consume('+'))
+      node = new_node(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_node(ND_SUB, node, mul());
+    else
+      return node;
+  }
+}
+
+Node *mul() {
+  Node *node = primary();
+
+  for (;;) {
+    if (consume('*'))
+      node = new_node(ND_MUL, node, primary());
+    else if (consume('/'))
+      node = new_node(ND_DIV, node, primary());
+    else
+      return node;
+  }
+}
+
+Node *primary() {
+  // If next token is "(", it should be "(" expr ")"
+  if (consume('(')) {
+    Node *node = expr();
+    expect(')');
+    return node;
+  }
+
+  // If not, it should be value
+  return new_node_num(expect_number());
+}
+
 struct Token {
   TokenKind kind;  // Token kind
   Token *next;     // Next token
